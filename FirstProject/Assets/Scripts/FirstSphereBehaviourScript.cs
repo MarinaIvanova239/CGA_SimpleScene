4256de92ab1caab107b2853cda8e01e5210b3c04
MarinaIvanova239 <mar.ivanova@bk.ru>
2017-02-19 13:29:39 +0300

    simple stage was finished

---
+++ FirstProject/Assets/Scripts/FirstSphereBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstSphereBehaviourScript : MonoBehaviour {
+    
+    public float maxHeight = 4.0f;
+    private float currentHeight = 0.0f;
+    public float step = 0.1f;
+    public Vector3 movement = new Vector3(0.0f, 0.1f, 0.0f);
+
+    void Update()
+    {
+        transform.Translate(movement);
+        currentHeight += step;
+        if (currentHeight >= maxHeight)
+        {
+            movement *= -1;
+            currentHeight = 0.0f;
+        }
+    }
+
+}
---
+++ FirstProject/Assets/Scripts/SecondSphereBehaviourScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondSphereBehaviourScript : MonoBehaviour {
+
+    public float rotationSpeed = 6.0f;
+
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.up, 360.0f / rotationSpeed * Time.deltaTime);
+    }
+}
